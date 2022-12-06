591c206b7937d9cc09e1ada54b3cc3603065c2fd
Franco Lorca <lorcafrancog@gmail.com>
2022-12-05 21:08:01 -0300

    Update
    Added functionality for change theme on press toggle key

---
+++ js/change-theme-class.js
@@ -0,0 +1,13 @@
+const $body = document.getElementsByTagName("body")[0];
+
+// Change Theme class
+const changeThemeClass = (newTheme, oldThemeOne, oldThemeTwo) => {
+    // Check if class already exists
+    if (!$body.classList.contains(newTheme)) {
+        // Remove other classes
+        $body.classList.remove(oldThemeOne, oldThemeTwo);
+        // Add class
+        $body.classList.add(newTheme)
+    }
+}
+export default changeThemeClass;
