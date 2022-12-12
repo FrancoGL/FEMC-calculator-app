6dca95acf2f01726018bfc22c85d51430e9a0cce
Franco Lorca <lorcafrancog@gmail.com>
2022-12-12 17:02:49 -0300

    Update
    Added functionality to save current theme

--- js/theme/change-theme-class.js
+++ js/theme/change-theme-class.js
@@ -2,12 +2,12 @@ const $body = document.getElementsByTagName("body")[0];
 
 // Change Theme class
 const changeThemeClass = (newTheme, oldThemeOne, oldThemeTwo) => {
-    // Check if class already exists
-    if (!$body.classList.contains(newTheme)) {
-        // Remove other classes
-        $body.classList.remove(oldThemeOne, oldThemeTwo);
-        // Add class
-        $body.classList.add(newTheme)
-    }
-}
+  // Check if class already exists
+  if (!$body.classList.contains(newTheme)) {
+    // Remove other classes
+    $body.classList.remove(oldThemeOne, oldThemeTwo);
+    // Add class
+    $body.classList.add(newTheme);
+  }
+};
 export default changeThemeClass;
